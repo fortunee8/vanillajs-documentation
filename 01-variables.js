888c198e1710bc0b17e6c41c33cbf7946d6ba3d8
Lucky Setiawan <101236568+fortunee8@users.noreply.github.com>
2023-02-06 00:09:12 +0700

    Learn day 1

---
+++ 01-variables.js
@@ -0,0 +1,10 @@
+// We'll learn variable rules in Javascript
+
+var myName = 'Lucky'; //This is string data type
+var myAge = 20; //integer data type
+var isLife = true; // boolean data type
+
+//Let's check the variable via console
+console.log(myName); //Mention the variable instead a variable value
+console.log(myAge); 
+console.log(isLife);
