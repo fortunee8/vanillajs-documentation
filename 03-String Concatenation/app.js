99546f55b1901970230dcbb0430305d28f357cc7
Lucky Setiawan <101236568+fortunee8@users.noreply.github.com>
2023-02-08 13:30:15 +0700

    day 4 string concatenation

---
+++ 03-String Concatenation/app.js
@@ -0,0 +1,7 @@
+//Data type
+myName = "Lucky"
+myAge = 20;
+myHobby = "Coding"
+
+//String concatenation, just call the variable
+console.log(`Hello my name is ${myName} I'm ${myAge} years old, my hobby is ${myHobby}`);
