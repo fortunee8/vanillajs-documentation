e99f1df05c4b112758ce200b515991168332397a
Lucky Setiawan <101236568+fortunee8@users.noreply.github.com>
2023-02-08 14:57:19 +0700

    day 4 part 2 if else

---
+++ 04-If Else/app.js
@@ -0,0 +1,11 @@
+//Data type
+
+myAge = 22
+internReq = 21
+
+if (myAge > internReq){
+    console.log("Yes, you're accepted in internship program!");
+}
+else {
+    console.log("Sorry, your age isn't enought for this position");
+}
