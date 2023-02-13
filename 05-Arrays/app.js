f70ea09283328724d72a68440ede64aeb2c18933
Lucky Setiawan <101236568+fortunee8@users.noreply.github.com>
2023-02-13 19:51:01 +0700

    learn array javascript

---
+++ 05-Arrays/app.js
@@ -0,0 +1,15 @@
+//Array declaration
+const namaBuah = ['Apel', 'Jeruk', 'Nanas', 'Melon', 'Anggur'];
+
+//Show the array value into console
+console.log(namaBuah);
+
+//Add array element into index
+namaBuah.push('pisang', 'mangga', 'semangka');
+//Check the array lenght after pushing
+console.log(namaBuah.length);
+
+//Remove array's index from memory
+namaBuah.pop('Apel', 'Nanas');
+//Check the array's lenght after popping
+console.log(namaBuah.length);
